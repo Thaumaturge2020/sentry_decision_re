c10ebcc5da4f45860e502610614702097925615e
Kewu Yang <115705247+Thaumaturge2020@users.noreply.github.com>
2024-05-27 19:06:04 +0800

    Update operator_node.cpp

--- src/node_library/src/operator_node.cpp
+++ src/node_library/src/operator_node.cpp
@@ -13,17 +13,17 @@ namespace BehaviorTree{
                     subscription_operator_cmd = node1->create_subscription<robot_msgs::msg::AerialCommands>("/easy_robot_commands/map_command",10,[this](const robot_msgs::msg::AerialCommands &msg){
                         switch(msg.cmd_keyboard){
                             //控符
-                            case 'T': aerial_type = 0;break;
+                            case 'W': aerial_type = 0;break;
                             //强制重启
-                            case 'Y': aerial_type = 1;break;
+                            case 'S': aerial_type = 1;break;
                             //强制定点移动
-                            case 'U': aerial_type = 2;break;
+                            case 'A': aerial_type = 2;break;
                             //强制回巡逻区巡逻
-                            case 'I': aerial_type = 3;break;
+                            case 'D': aerial_type = 3;break;
                             //自主买弹
-                            case 'P': aerial_type = 5;break;
+                            case 'B': aerial_type = 5;break;
                             //哨兵进攻基地
-                            case 'L': aerial_type = 6;break;
+                            case 'E': aerial_type = 6;break;
                             //取消选择
                             case 'Q': aerial_type = -1;break;
                         }
